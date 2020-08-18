64916143833c65cd9d11657825216e37950752ed
kai <839823628@qq.com>
2020-08-18 15:21:17 +0800

    用户现实页面

--- routes/web.php
+++ routes/web.php
@@ -23,3 +23,12 @@ Route::get('/about','StaticPagesController@about')->name('about');
 
 
 Route::get('signup','UserController@create')->name('signup');
+
+Route::resource('users','UserController');
+//Route::get('/users', 'UsersController@index')->name('users.index');
+//Route::get('/users/create', 'UsersController@create')->name('users.create');
+//Route::get('/users/{user}', 'UsersController@show')->name('users.show');
+//Route::post('/users', 'UsersController@store')->name('users.store');
+//Route::get('/users/{user}/edit', 'UsersController@edit')->name('users.edit');
+//Route::patch('/users/{user}', 'UsersController@update')->name('users.update');
+//Route::delete('/users/{user}', 'UsersController@destroy')->name('users.destroy');
