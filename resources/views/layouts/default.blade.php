bb72d36b924d0017f25e0b8ad3063f647786fb57
kai <839823628@qq.com>
2020-08-18 16:09:14 +0800

    登陆成功后导航逻辑

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -1,14 +1,21 @@
+<!DOCTYPE html>
 <html>
 <head>
     <title>@yield('title', 'Weibo App') - Laravel 入门教程</title>
     <link rel="stylesheet" href="{{ mix('css/app.css') }}">
 </head>
+
 <body>
 @include('layouts._header')
+
 <div class="container">
-    @include('shared._message')
-    @yield('content')
-@include('layouts.__footer')
+    <div class="offset-md-1 col-md-10">
+        @include('shared._message')
+        @yield('content')
+        @include('layouts.__footer')
+    </div>
 </div>
+
+<script src="{{ mix('js/app.js') }}"></script>
 </body>
 </html>
